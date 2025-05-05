f48b704ca7263508ce48c449dfe7361564624112
SurajPrasaad <surajkameshwarprasad@gmail.com>
2025-05-05 12:20:43 +0530

    cors intigreation

--- AuthMongoDb/index.js
+++ AuthMongoDb/index.js
@@ -1,9 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors({
+    origin:"http:localhost:8000",
+    credentials:true,
+    methods:['GET','POST','DELETE','OPTIONS'],
+    allowedHeaders:['Content-Type','Authorization']
+}))
 const PORT = process.env.PORT || 8000;
 
 app.get('/',(request,response)=>{
