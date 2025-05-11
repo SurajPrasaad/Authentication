63b2159e45171b68df8da36e2b04d477530cfa40
SurajPrasaad <surajkameshwarprasad@gmail.com>
2025-05-11 09:02:28 +0530

    feat(auth): initialize authentication with MySQL integration

---
+++ AuthMySql/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(cors({
+    origin:"*",
+    credentials:true,
+    methods:["GET","POST","PATCH","PUT"],
+    allowedHeaders:["Content-Type","Authorization"]
+}))
+app.get("/",(req,res)=>{
+    res.send({message:"Welcome"})
+})
+
+app.listen(port,()=>{
+    console.log(`App is listening on ${port}`)
+})
