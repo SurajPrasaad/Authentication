e6f7159a2822edf97340df45fad597e22faed077
SurajPrasaad <surajkameshwarprasad@gmail.com>
2025-05-15 10:13:24 +0530

    feat: integrated Neon DB with Prisma for database management

--- AuthPrisma/controllers/auth.controller.js
+++ AuthPrisma/controllers/auth.controller.js
@@ -1,3 +1,22 @@
-export const registerUser = async ()=>{
-    console.log("Registered User....");
-}
+import { PrismaClient } from '../generated/prisma/index.js';
+
+// import pkg from '@prisma/client';
+// const { PrismaClient } = pkg;
+
+const prisma = new PrismaClient();
+
+export const registerUser = async () => {
+  try {
+    console.log("Registering User...");
+    const user = await prisma.user.create({
+      data: {
+        name: "Suraj",
+        email: "surajprasad31100874@gmail.com",
+        password: "12450"
+      }
+    });
+    console.log("User Created", user);
+  } catch (err) {
+    console.error("Error creating user:", err.message);
+  }
+};
